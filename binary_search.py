b9bb5f5e401b14e43e7d7f1ada5269374bbbf73c
steveostler <stephenjostler@gmail.com>
2024-03-19 13:44:57 +0000

    added comments

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,18 @@
 def binary_search(arr, item):
+    # low and high keep track of which part of the list you'll search in
     low = 0
     high = len(arr)-1
 
-    while low <= high:
-        mid = (low + high) // 2
+    while low <= high: # while not narrowed down to 1 element
+        mid = (low + high) // 2 # check the middle element
         guess = arr[mid]
-        if guess == item:
+        if guess == item: # found the item
             return mid
-        elif guess > item:
+        elif guess > item: # too high
             high = mid - 1
         else:
-            low = mid + 1
-    return None
+            low = mid + 1 # too low
+    return None # does not exist
 
 my_list = [1,3,5,7,9,11]
 
